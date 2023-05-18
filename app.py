8149c240cf587b13ce6967a080fa5b0f600fb964
speedcoder107 <85942485+speedcoder107@users.noreply.github.com>
2023-05-18 21:06:53 +0530

    created style, js, flask, templates folder

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd, update
+
+# Configure application
+app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
