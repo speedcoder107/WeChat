160ff454601999da4d2c2bab84de871ed54f87bb
speedcoder107 <85942485+speedcoder107@users.noreply.github.com>
2023-05-18 15:59:17 +0000

    updated style and index

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd, update
+# from tempfile import mkdtemp
+# from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
